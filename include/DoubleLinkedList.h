38a6d756a1000ef67212693bb16f025a3960c518
Andrieieva <kristinaandrieieva13@gmail.com>
2026-03-24 09:54:44 +0100

    need to fix add,allocate

---
+++ include/DoubleLinkedList.h
@@ -0,0 +1,18 @@
+//
+// Created by krist on 24.03.2026.
+//
+#ifndef DOUBLELINKEDLIST_H
+#define DOUBLELINKEDLIST_H
+
+#endif //DOUBLELINKEDLIST_H
+
+template<typename  T>
+class DoubleLinkedList {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node* prev;
+    };
+
+};
