024c7349acb908df0d00deb5cf5de6b76395b393
Andrieieva <kristinaandrieieva13@gmail.com>
2026-04-17 19:17:40 +0200

    Added Controller.h To modify runBenchmark, runSingleFile(Controller.h)

--- include/Controller.h
+++ include/Controller.h
@@ -6,23 +6,135 @@
 #define CONTROLLER_H
 
 
-#include <iostream>
-#include <chrono>
-#include <fstream>
-#include <string>
-
 #include "Parameters.h"
 #include "FileService.h"
-#include "Array.h"
-#include "SingleLinkedList.h"
-#include "DoubleLinkedList.h"
 #include "QuickSort.h"
 #include "ShellSort.h"
 #include "BucketSort.h"
+#include <chrono>
+#include <iostream>
 
 using namespace std;
+template<typename  T>
 class Controller {
-    public:
+public:
+    static void run() {
+        switch (Parameters::dataType) {
+            case Parameters::DataTypes::typeInt:          runMode<int>(); break;
+            case Parameters::DataTypes::typeFloat:        runMode<float>(); break;
+            case Parameters::DataTypes::typeDouble:       runMode<double>(); break;
+            case Parameters::DataTypes::typeString:       runMode<std::string>(); break;
+            default: std::cerr << "Nie ma takiego typu danych w programie!" << std::endl;
+        }
+    }
 
+private:
+    template <typename T>
+    static void runMode() {
+        if (Parameters::runMode == Parameters::RunModes::singleFile) {
+            runSingleFile<T>();
+        } else if (Parameters::runMode == Parameters::RunModes::benchmark) {
+            runBenchmark<T>();
+        }
+    }
+
+
+    template <typename T>
+    static void runSingleFile() {
+        if (Parameters::inputFile.empty()) {
+            std::cerr << "Blad!" << std::endl;
+            return;
+        }
+
+        if (Parameters::structure == Parameters::Structures::array) {
+            Array<T>* array = FileService<T>::loadDataArray(Parameters::inputFile);
+            if (array == nullptr) return;
+
+            std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+            SortOnArray<T>(*array);
+
+            std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double, std::milli> elapsed = end - start;
+            std::cout << "Czas wykonania: " << elapsed.count() << " ms" << std::endl;
+            delete array;
+        }
+
+        else if (Parameters::structure == Parameters::Structures::singleList) {
+            SingleLinkedList<T>* slist = FileService<T>::loadDataSingleLinkedlist(Parameters::inputFile);
+            if (slist == nullptr) return;
+
+            std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+            SortOnSingleList<T>(*slist);
+
+            std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double, std::milli> elapsed = end - start;
+            std::cout << "Czas wykonania: " << elapsed.count() << " ms" << std::endl;
+            delete slist;
+
+        }else if(Parameters::structure == Parameters::Structures::doubleList) {
+            DoubleLinkedList<T>* dlist = FileService<T>::loadDataDoubleLinkedlist(Parameters::inputFile);
+            if (dlist == nullptr) return;
+
+            std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+
+            SortOnDoubleList<T>(*dlist);
+
+            std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double, std::milli> elapsed = end - start;
+            std::cout << "Czas wykonania: " << elapsed.count() << " ms" << std::endl;
+            delete dlist;
+        }
+    }
+
+
+    template <typename T>
+    static void SortOnArray(Array<T>& array) {
+        if (Parameters::algorithm == Parameters::Algorithms::quick) {
+            QuickSort<T>::quickSortArray(array, getPivotStr(), 0, array.getSize() - 1);
+        } else if (Parameters::algorithm == Parameters::Algorithms::shell) {
+            ShellSort<T>::shellSortArray(array, getGapStr());
+        } else if (Parameters::algorithm == Parameters::Algorithms::bucket) {
+            BucketSort<T>::bucketSortArray(array);
+        }
+    }
+
+    static void SortOnSingleList(SingleLinkedList<T>& slist) {
+        if (Parameters::algorithm == Parameters::Algorithms::quick) {
+            QuickSort<T>::quickSortSingleList(slist, getPivotStr(), 0, slist.getSize() - 1);
+        } else if (Parameters::algorithm == Parameters::Algorithms::shell) {
+            ShellSort<T>::shellSortSingleList(slist, getGapStr());
+        } else if (Parameters::algorithm == Parameters::Algorithms::bucket) {
+            BucketSort<T>::bucketSortSingleList(slist);
+        }
+    }
+
+
+    static void SortOnDoubleList(SingleLinkedList<T>& dlist) {
+        if (Parameters::algorithm == Parameters::Algorithms::quick) {
+            QuickSort<T>::quickSortDoubleList(dlist, getPivotStr(), 0, dlist.getSize() - 1);
+        } else if (Parameters::algorithm == Parameters::Algorithms::shell) {
+            ShellSort<T>::shellSortDoubleList(dlist, getGapStr());
+        } else if (Parameters::algorithm == Parameters::Algorithms::bucket) {
+            BucketSort<T>::bucketSortDoubleList(dlist);
+        }
+    }
+
+    static std::string getPivotStr() {
+        if (Parameters::pivot == Parameters::Pivots::left) return "Left";
+        if (Parameters::pivot == Parameters::Pivots::middle) return "Centre";
+        if (Parameters::pivot == Parameters::Pivots::random) return "Random";
+        return "Right";
+    }
+
+    static std::string getGapStr() {
+        return (Parameters::shellParameter == Parameters::ShellParameters::option2) ? "Option 2" : "Option 1";
+    }
+
+    template <typename T>
+    static void runBenchmark() {
+        std::cout << "Uruchamiam benchmark..." << std::endl;
+    }
 };
 #endif //CONTROLLER_H
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "FileService.h"
 #include "QuickSort.h"
 #include "BucketSort.h"
+#include "Controller.h"
 
 
 #include "Parameters.h"
@@ -30,120 +31,6 @@ int main(int argc, char** argv) {
         return 0;
     }
 
+    Controller::run();
 
-    if (Parameters::runMode == Parameters::RunModes::singleFile) {
-        if (Parameters::inputFile.empty()) {
-            cerr << "Blad: Nie podano pliku wejsciowego (-i)!" << endl;
-            return 1;
-        }
-
-
-        if (Parameters::dataType == Parameters::DataTypes::typeInt) {
-            if (Parameters::structure == Parameters::Structures::array) {
-                Array<int>* array = FileService<int>::loadDataArray(Parameters::inputFile);
-
-                if (array == nullptr) return 1;
-
-                if (Parameters::algorithm == Parameters::Algorithms::quick) {
-                    string pStr = "Centre";
-                    if(Parameters::pivot == Parameters::Pivots::left) pStr = "Left";
-                    if(Parameters::pivot == Parameters::Pivots::random) pStr = "Random";
-
-                    QuickSort<int>::quickSortArray(*array, pStr, 0, array->getSize() - 1);
-                }
-                else if (Parameters::algorithm == Parameters::Algorithms::bucket) {
-                    BucketSort<int>::bucketSortArray(*array);
-                }else if (Parameters::algorithm == Parameters::Algorithms::shell) {
-                    string gap = "Option 1";
-                    if (Parameters::shellParameter == Parameters::ShellParameters::option2) {
-                        gap = "Option 2";
-                    }
-                    ShellSort<int>::shellSortArray(*array,gap);
-                }
-
-                for (int i = 0; i < array->getSize(); i++) {
-                    cout << (*array)[i] << " ";
-                }
-                cout << endl;
-
-                delete array;
-            }else if (Parameters::structure == Parameters::Structures::singleList) {
-                SingleLinkedList<int>* slist = FileService<int>::loadDataSingleLinkedlist(Parameters::inputFile);
-                if (slist == nullptr) return 1;
-
-                if (Parameters::algorithm == Parameters::Algorithms::quick) {
-
-                    string pStr = "Right";
-                    if (Parameters::pivot == Parameters::Pivots::left) pStr = "Left";
-                    else if (Parameters::pivot == Parameters::Pivots::middle) pStr = "Centre";
-                    else if (Parameters::pivot == Parameters::Pivots::random) pStr = "Random";
-
-
-                    if (slist->head != nullptr) {
-                        QuickSort<int>::quickSortSingleList(*slist, pStr, slist->head, slist->getTail());
-                    }
-                }else if (Parameters::algorithm == Parameters::Algorithms::bucket) {
-                    if (slist->head != nullptr) {
-                        BucketSort<int>::bucketSortSingleList(*slist);
-                    }
-                }else if (Parameters::algorithm == Parameters::Algorithms::shell) {
-                    string gap = "Option 1";
-                    if (Parameters::shellParameter == Parameters::ShellParameters::option2) {
-                        gap = "Option 2";
-                    }
-                    ShellSort<int>::shellSortSingleList(*slist, gap);
-                }
-
-                SingleLinkedList<int>::Node* curr = slist->head;
-                while (curr != nullptr) {
-                    cout << curr->data << " ";
-                    curr = curr->next;
-                }
-                cout << endl;
-
-                delete slist;
-
-            }else if (Parameters::structure == Parameters::Structures::doubleList) {
-                DoubleLinkedList<int>* dlist = FileService<int>::loadDataDoubleLinkedlist(Parameters::inputFile);
-                if (dlist == nullptr) return 1;
-
-                if (Parameters::algorithm == Parameters::Algorithms::quick) {
-
-                    string pStr = "Right";
-                    if (Parameters::pivot == Parameters::Pivots::left) pStr = "Left";
-                    else if (Parameters::pivot == Parameters::Pivots::middle) pStr = "Centre";
-                    else if (Parameters::pivot == Parameters::Pivots::random) pStr = "Random";
-
-
-                    if (dlist->head != nullptr) {
-                        QuickSort<int>::quickSortDoubleList(*dlist, pStr, dlist->head, dlist->getTail());
-                    }
-                }else if (Parameters::algorithm == Parameters::Algorithms::bucket) {
-                    if (dlist->head != nullptr) {
-                        BucketSort<int>::bucketSortDoubleList(*dlist);
-                    }
-                }else if (Parameters::algorithm == Parameters::Algorithms::shell) {
-                    string gap = "Option 1";
-                    if (Parameters::shellParameter == Parameters::ShellParameters::option2) {
-                        gap = "Option 2";
-                    }
-                    ShellSort<int>::shellSortDoubleList(*dlist, gap);
-                }
-
-                DoubleLinkedList<int>::Node* curr = dlist->head;
-                while (curr != nullptr) {
-                    cout << curr->data << " ";
-                    curr = curr->next;
-                }
-                cout << endl;
-
-                delete dlist;
-            }
-        }
-        else if (Parameters::runMode == Parameters::RunModes::benchmark) {
-            cout << "Tryb benchmark nie jest jeszcze w pelni zaimplementowany." << endl;
-        }
-
-        return 0;
-    }
 };
